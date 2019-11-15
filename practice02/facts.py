cffab6d5f13f197dd5b204354f9ec19c1068742d
Alexander Morozov <scanhex@gmail.com>
2019-11-15 12:54:19 +0300

    Added myself.

--- practice02/facts.py
+++ practice02/facts.py
@@ -5,8 +5,9 @@ import sys
 
 Person = collections.namedtuple('Person', ['github_login', 'first_name', 'last_name', 'facts'])
 people = [
-    Person(github_login='example', first_name='Name', last_name='Surname',
-           facts=['Fact-right.', 'Fact-write.', 'Fact-right.']),
+    Person(github_login='scanhex', first_name='Alexander', last_name='Morozov',
+           facts=['Nearly 30,000 rubber ducks were lost a sea in 1992 and are still being discovered today', 'The world\'s largest pyramid isn\'t in Egypt.
+               ', 'Different Parts of Your Tongue Detect Different Tastes.']),
 ]
 
 answers = {}
